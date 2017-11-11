83bbd40c6f4fafcf2d7e154c5f5782eaaad765c1
AlexisPerrot <alexis.perrot@etu.univ-lyon1.fr>
2017-11-11 02:06:11 +0100

    Determinize : Premiers tests
    Création de l'ensemble intermédiaire d'états, de l'automate final, affichage des symboles utilisés, etc.

--- liflf-projet-2017/src/Determinize.cpp
+++ liflf-projet-2017/src/Determinize.cpp
@@ -6,14 +6,38 @@
 #include "FA_tools.h"
 #include "Accept.h"
 
+using namespace std;
+
 ////////////////////////////////////////////////////////////////////////////////
 
-sAutoNDE Determinize(const sAutoNDE& at){
-  //TODO définir cette fonction
+sAutoNDE Determinize(const sAutoNDE& at)
+{
+    if (EstDeterministe(at)){
+        return at;
+    }else{
+        unsigned int i,j;
+        etatset_t nouveauEtats;
+        set<etat_t>::const_iterator itr;
+        sAutoNDE atD;
 
-  sAutoNDE r;
 
-  return r;
+        Fermeture(at, nouveauEtats); ///Obligé de faire la fermeture une fois au début car la fonction ne renvoie pas la fermeture mais complète l'automate, donc il faut compléter l'automate de départ au début
+        nouveauEtats.insert(at.initial); ///Insertion de l'état 0, considéré comme l'état de départ
+
+
+        for (itr = nouveauEtats.begin() ; itr != nouveauEtats.end() ; itr++){
+            for ( i = 0 ; i<at.nb_symbs ; i++){
+                cout << "Symbole : " << (char)(ASCII_A + i) << endl;
+            }
+        }
+
+
+        atD.initial = at.initial;
+        cout << "Nouvel etat initial : " << atD.initial << endl;
+        //TODO : Ajouter les états finaux et le nombre d'états
+
+        return atD;
+    }
 }
 
 //******************************************************************************
