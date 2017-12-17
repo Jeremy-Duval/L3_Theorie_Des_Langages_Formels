2aa952286dd4f03b44f2a14c06449b82b08adef3
Jeremy Duval <jeremy-duval1@hotmail.fr>
2017-12-17 16:57:01 +0100

    -Attention : correction erreur arrangement automate : at.epsilon : 	- oublie d'incrémentation des etat atteind par E-trans 	- correction de l'ajout d'E-trans vers etat final en at.nb_etat-1 (manquait le -1) -Début implémentation passage en expression rationnelle (manque transition[i][j]=D|A.C*.B

--- liflf-projet-2017/src/Accept.cpp
+++ liflf-projet-2017/src/Accept.cpp
@@ -198,10 +198,13 @@ std::cout << "****************************************" <<std::endl;
     /* CODER DANS ACCEPT ; A LA FIN ! */
 
   std::string sr;
-  unsigned int j;
+  unsigned int j, k;
   std::vector<etatset_t> tmp_vect_es;
   etatset_t tmp_es, tmp_copie_es;
-    //int i;
+  std::vector<std::vector<std::string>> transitions;
+  std::vector<std::string> tmp_vect_str;
+  bool trans_ijk;
+    //unsigned int i;
 
   std::cout << "nb_etats : " << at.nb_etats <<std::endl;
   std::cout << "nb_symbs : " << at.nb_symbs <<std::endl;
@@ -228,6 +231,30 @@ std::cout << "****************************************" <<std::endl;
     at.trans.insert(at.trans.begin(),tmp_vect_es);
     std::cout<<"at.trans : "<<at.trans<<std::endl;
 
+    //décalage des e-trans
+    auto itr_ie=at.epsilon.begin();
+    i=0;
+    while(itr_ie<at.epsilon.end())
+    {
+
+            //init etat_set
+            tmp_copie_es.clear();
+
+            auto itr_set=at.epsilon[i].begin();
+            while(itr_set!=at.epsilon[i].end())
+            {
+                tmp_copie_es.insert(itr_set,*itr_set+1);
+                itr_set++;
+
+            }
+
+        //clean at.trans[i][j]
+        at.epsilon.erase(itr_ie);
+        at.epsilon.insert(itr_ie,tmp_copie_es);
+        i++;
+        itr_ie++;
+    }
+    std::cout << "epsilon : " << at.epsilon <<std::endl;
     //on ajoute la E-transition du nouvel état de départ vers l'ancien
     tmp_es.insert(at.initial+1);
     at.epsilon.insert(at.epsilon.begin(), tmp_es);
@@ -293,7 +320,7 @@ std::cout << "****************************************" <<std::endl;
     auto itr_finaux=at.finaux.begin();
     while(itr_finaux!=at.finaux.end())
     {
-        at.epsilon[*itr_finaux+1].insert(at.epsilon[*itr_finaux+1].end(),at.nb_etats);//le +1 est là à cause du décalage fait précedemment pour ajouter l'état de départ générique ; at.trans et at.epsilon ont été mis à jour mais pas at.finaux
+        at.epsilon[*itr_finaux+1].insert(at.epsilon[*itr_finaux+1].end(),at.nb_etats-1);//le +1 est là à cause du décalage fait précedemment pour ajouter l'état de départ générique ; at.trans et at.epsilon ont été mis à jour mais pas at.finaux
         itr_finaux++;
     }
     std::cout << "epsilon : " << at.epsilon <<std::endl;
@@ -312,8 +339,88 @@ std::cout << "****************************************" <<std::endl;
     /* *******Construction de l'expression rationnelle ******* */
     /* ******************************************************* */
 
-    std::cout<<"Sr : "<<sr<<std::endl;
+    //initialisation de la matrice
+    //-1
+    i=0;
+    while(i<at.nb_etats)
+    {
+        tmp_vect_str.push_back("-1");
+        i++;
+    }
+    i=0;
+    while(i<at.nb_etats)
+    {
+        transitions.push_back(tmp_vect_str);
+        i++;
+    }
+    //ajout des transition
+    i=0;
+    while(i<at.nb_etats)
+    {
+        j=0;
+        while(j<at.nb_symbs)
+        {
+            auto itr_set=at.trans[i][j].begin();
+            while(itr_set!=at.trans[i][j].end())
+            {
+                transitions[i][*itr_set]=j+ASCII_A;
+                itr_set++;
 
+            }
+            j++;
+        }
+        i++;
+    }
+    //ajout des e-transition
+    i=0;
+    while(i<at.nb_etats)
+    {
+        auto itr_set=at.epsilon[i].begin();
+        while(itr_set!=at.epsilon[i].end())
+        {
+            transitions[i][*itr_set]="E";
+            itr_set++;
+        }
+        i++;
+    }
+
+    std::cout << "transitions: " << transitions <<std::endl;
+
+    i=0; //après arrangement, l'état initial sera toujours 0
+
+    k=1;
+    while(k<at.nb_etats-1)//k symbolise les état supprimables, on ne doit pas prendre le dernier, ni le premier
+    {
+        j=1;
+        while(j<at.nb_etats)
+        {
+            if(j!=k)
+            {
+                //test l'existance d'une transition de i à j par k
+                trans_ijk = false;
+                if(transitions[i][k]!="-1")
+                {
+                    if(transitions[k][j]!="-1")
+                    {
+                        trans_ijk=true;
+                    }
+                }
+                if(trans_ijk)
+                {
+                    //transitions[i][j]=D|A.C*.B;
+                }
+                transitions[k][j]="-1";
+            }// end if(j!=k)
+            j++;
+        }// end while(j<at.nb_etats)
+        transitions[k][k]=-1; //transition d'un etat vers lui-même
+        k++;
+    }// end while(k<at.nb_etats-1)
+
+    sr = transitions[0][at.nb_etats-1];
+
+
+    std::cout<<"Sr : "<<sr<<std::endl;
 
 
 
