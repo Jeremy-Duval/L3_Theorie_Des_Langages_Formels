7f7bd0b7dd3b90f549078014dabd890cfdb5f1a9
AlexisPerrot <alexis.perrot@etu.univ-lyon1.fr>
2017-12-22 23:58:53 +0100

    Minimize presque finie

--- liflf-projet-2017/src/Determinize.cpp
+++ liflf-projet-2017/src/Determinize.cpp
@@ -9,7 +9,79 @@
 using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
+sAutoNDE Determinize(const sAutoNDE& at){
+    if(!EstDeterministe(at)){
+        //Initialisation de l'automate de travail
+        sAutoNDE r;
+        r.initial = 0;
+        r.nb_etats=1;
+        r.nb_symbs=at.nb_symbs;
+        r.nb_finaux=0;
 
+        etatset_t prec;
+        etatset_t cour;
+        std::vector<etatset_t> nouvVector;
+        std::vector<etatset_t> etatCree;
+
+        prec.insert(at.initial);
+            Fermeture(at, prec);
+
+        etatCree.push_back(prec);
+
+        for(int indexExit= 0; (unsigned int)indexExit < etatCree.size(); indexExit++){
+            r.trans.push_back(nouvVector);
+            prec = etatCree[indexExit];
+            Fermeture(at,prec);
+            for ( size_t i = 0; i < at.nb_symbs ; i++){
+                cour = Delta(at, prec, (char) ASCII_A+i);
+                Fermeture(at,cour);
+                auto itEC = etatCree.begin();
+                bool existe = false;
+                etat_t id;
+                for ( id = 0 ; id<(etat_t)etatCree.size() ; id++ ){
+
+                    if (cour == *itEC){
+
+                      etatset_t nouvEtat;
+                      nouvEtat.insert(id);
+                      r.trans[indexExit].push_back(nouvEtat);
+                      itEC++;
+                      existe=true;
+                      break;
+
+                    }
+                itEC++;
+
+                }
+                if (!existe){
+
+                  etatset_t nouvEtat;
+                  nouvEtat.insert(id);
+                  etatCree.push_back(cour);
+                  r.trans[indexExit].push_back(nouvEtat);
+                  r.nb_etats++;
+
+                }
+              }
+
+
+                }
+          for(int i=0; i < (int)etatCree.size();i++){
+                     for(auto it=etatCree[i].begin(); it!=etatCree[i].end();it++){
+                             for(auto itT=at.finaux.begin(); itT!=at.finaux.end();itT++){
+                                      if(*it==*itT) {
+                                                r.finaux.insert((etat_t)i);
+                        }
+                    }
+                }
+            }
+              r.nb_etats = r.trans.size();
+              r.nb_finaux = r.finaux.size();
+          return r;
+    }else return at;
+
+
+/*
 sAutoNDE Determinize(const sAutoNDE& at)
 {
     sAutoNDE atD;
@@ -82,6 +154,6 @@ sAutoNDE Determinize(const sAutoNDE& at)
         }
     }
     return atD;
-}
+}*/
 
 //******************************************************************************
--- liflf-projet-2017/src/Minimize.cpp
+++ liflf-projet-2017/src/Minimize.cpp
@@ -6,15 +6,103 @@
 #include <algorithm>
 
 #include "Minimize.h"
+#include "Determinize.h"
+#include "Accept.h"
+using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-sAutoNDE Minimize(const sAutoNDE& at){
-  //TODO définir cette fonction
+sAutoNDE Minimize(const sAutoNDE& aOrigine)
+{
+    ///Initialisation
+    sAutoNDE aMinimal;
+    vector<etatset_t> VeSAvant;
+    vector<etatset_t> VeSApres;
+    etatset_t finaux;
+    etatset_t nonFinaux;
+    map<etat_t,vector<etatset_t> > transMap;
 
-  sAutoNDE r;
+    ///On déterminise l'automate
+    if(!EstDeterministe(aOrigine))Determinize(aOrigine);
 
-  return r;
+    ///On sépare les états en finaux/non-finaux
+    VeSAvant.insert(VeSAvant.end(),aOrigine.finaux);
+    for(etat_t etat = 0 ; etat<aOrigine.nb_etats ; etat++){
+        if(finaux.find(etat) != finaux.end()) {
+            nonFinaux.insert(nonFinaux.end(),etat);
+        }
+    }
+    do{
+
+        vector<etatset_t>::iterator etatsetIt;
+        for(etatsetIt = VeSAvant.begin() ; etatsetIt != VeSAvant.end() ; etatsetIt++){
+            ///On associe chaque état au "groupe d'état" qu'il peut atteindre
+            etatset_t etatSetEnCours = *etatsetIt;
+            etatset_t::iterator etatIt;
+            for (etatIt = etatSetEnCours.begin() ; etatIt != etatSetEnCours.end() ; etatIt++){
+                vector<etatset_t> etatSetsAtteignables;
+                for(int numSymbol=0 ; numSymbol < aOrigine.nb_symbs ; numSymbol++){
+                    symb_t symboleEnCours = ASCII_A + numSymbol;
+                    etatSetsAtteignables.insert(etatSetsAtteignables.end(),Delta(aOrigine,*etatsetIt,symboleEnCours));
+                }
+                pair<etat_t,vector<etatset_t> > transitionsEtat;
+                transitionsEtat.first = *etatIt;
+                transitionsEtat.second = etatSetsAtteignables;
+                transMap.insert(transMap.begin(),transitionsEtat);
+            }
+        }
+        etatset_t classe;
+        while(!transMap.empty()){
+            classe.clear();
+            ///On va regrouper les états par classes d'équivalences, on commence par sortir le premier état de la transMap
+            pair<etat_t,vector<etatset_t> > premieresTransitions = *(transMap.begin());
+            etat_t premierEtat = premieresTransitions.first;
+            classe.insert(classe.end(),premierEtat);
+            transMap.erase(transMap.begin());
+            ///Et on le compare à ceux restant
+            map<etat_t,vector<etatset_t> >::iterator transIt = transMap.begin();
+            while(transIt != transMap.end()){
+                pair<etat_t,vector<etatset_t> > pairEnCours = *transIt;
+                ///Si les deux états ont des transitions équivalentes, on les regroupe et on allège la transMap
+                if(premieresTransitions.second == pairEnCours.second){
+                    classe.insert(classe.end(),pairEnCours.first);
+                    transMap.erase(pairEnCours.first); //erase() incrémente tout seul après avoir supprimé la paire
+                }else{
+                    transIt++;
+                }
+            }
+            ///On ajoute alors la classe ainsi trouvée à notre nouvelle liste d'etatSets
+            VeSApres.insert(VeSApres.end(),classe);
+            /*(map<etat_t,vector<etatset_t> >::iterator transIt = transMap.begin() ; transIt != transMap.end() ; transIt++){
+                pair<etat_t,vector<etatset_t> > pairEnCours = *transIt;
+                ///Si les deux états ont des transitions équivalentes, on les regroupe
+                if(premieresTransitions.second == pairEnCours.second){
+                    classe.insert(classe.end(),pairEnCours.first);
+                }
+            }*/
+
+        }
+    }while(VeSAvant != VeSApres);
+
+    ///Assemblage de l'automate minimal
+    aMinimal.initial = aOrigine.initial;
+    aMinimal.nb_symbs = aOrigine.nb_symbs;
+    map<int, etatset_t> groupeEtats;
+    int i=0;
+    ///On associe à chaque nouvel classe un numéro dont on va se servir pour créer les états de l'automate minimal
+    for(vector<etatset_t>::iterator etatSetIt = VeSApres.begin() ; etatSetIt != VeSApres.end() ; etatSetIt++){
+        pair<int,etatset_t> paireEnCours;
+        paireEnCours.first = i;
+        paireEnCours.second = *etatSetIt;
+        groupeEtats.insert(groupeEtats.end(),paireEnCours);
+
+        i++;
+    }
+    ///On parcours cette liste pour trouver les nouveaux états finaux
+    for(map<int,etatset_t>::iterator pairIt = groupeEtats.begin() )
+    aMinimal.nb_finaux = aMinimal.finaux.size();
+
+    return aMinimal;
 }
 
 //******************************************************************************
